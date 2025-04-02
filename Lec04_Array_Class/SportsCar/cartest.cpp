b3ef9a1934224b8d79387548433860f3c347b82c
olyly0101 <yeah3687@gmail.com>
2025-04-02 09:18:46 +0900

    Lec05-2

--- Lec04_Array_Class/SportsCar/cartest.cpp
+++ Lec04_Array_Class/SportsCar/cartest.cpp
@@ -1,13 +1,19 @@
-#include "Car.h"
+//#include "Car.h"
 #include <cstdio>
 #include <cstring>
+#include "SportsCar.h"
 
 int main()
 {
 	Car myCar(10, "K5", 2);
+	SportsCar mySecondCar;
+
 
 	myCar.whereAmI();
 	myCar.changeGear(3);
 	myCar.speedUp();
 	myCar.display();
+
+	mySecondCar.setTurbo(true);
+	mySecondCar.display();
 }
