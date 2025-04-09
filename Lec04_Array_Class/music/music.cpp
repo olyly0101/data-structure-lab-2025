3b6621974f7808c5b8997f78f5ce0d3042e63fec
olyly0101 <yeah3687@gmail.com>
2025-04-09 09:54:44 +0900

    musicLec06

--- Lec04_Array_Class/music/music.cpp
+++ Lec04_Array_Class/music/music.cpp
@@ -2,5 +2,24 @@
 
 int main()
 {
+	//music streaming service¸¦ »ý¼º
+	MusicStreamingService my_service("spotify");
 
+	my_service.addMusic("PolaroidLove", "ENHYPEN", "DIMENSION", 2022);
+	my_service.addMusic("Ditto", "NewJeans", "ALBUM", 2023);
+	my_service.addMusic("CAKE", "ITZY", "ALBUM", 2023);
+	my_service.addMusic("SundayMorning", "Maroon5", "ALBUM", 2023);
+
+	// search music by title
+	string music_title;
+	cout << "Enter the Music Title: ";
+	cin >> music_title;
+	Music* result = my_service.searchByTitle(music_title);
+	if (result != NULL) {
+		cout << "Found: " << result->getTitle() << " by " << result->getArtist() << endl;
+	}
+	else
+	{
+		cout << "not found" << endl;
+	}
 }
--- Lec04_Array_Class/music/music.h
+++ Lec04_Array_Class/music/music.h
@@ -6,6 +6,7 @@
 using namespace std;
 
 class Music {
+
 private:
 	string title;
 	string artist;
@@ -14,6 +15,7 @@ private:
 
 public:
 	// 생성자
+	// public 없이 쓰면 기본적으로 private임.
 	Music(string title, string artist, string album, int year) {
 		this->title = title;
 		this->artist = artist;
@@ -21,12 +23,50 @@ public:
 		this->year = year;
 	}
 
+	// Getters
+	string getTitle() {
+		return title;
+	}
+	string getArtist() {
+		return artist;
+	}
+	string getAlbum() {
+		return album;
+	}
+	int getYear() {
+		return year;
+	}
 };
 
 class MusicStreamingService {
 
-	//음악 찾아주는 서비스
-	//음악 정보를 추가할 수 있는 기능
+private:
+	string service_name;
+	vector<Music> music_list;
 
+public:
+	//생성자
+	MusicStreamingService(string service_name) {
+		this->service_name = service_name;
+	}
+	// 음악 정보를 추가할 수 있는 기능
+	void addMusic(string title, string artist, string album, int year) {
+		Music new_music(title, artist, album, year);
+		music_list.push_back(new_music);
+		cout << title << " by " << artist << " added to " << service_name << endl;
+	}
+
+	// 음악 찾아주는 서비스 (Title)
+	// *(포인터)가 붙어있는 이유는 i값을 주소값으로 인지시켜주기 때문임.
+	Music* searchByTitle(string title) {
+		for (int i = 0; i < music_list.size(); i++) {
+			if (music_list[i].getTitle() == title) {
+				return &music_list[i];
+			}
+		}
+		return NULL;
+	}
+
+	// 음악 찾아주는 서비스 (Artist)
 
 };
