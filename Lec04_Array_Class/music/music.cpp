690d7c371e7d28cf86b57faf31730271f7b8e2ca
olyly0101 <yeah3687@gmail.com>
2025-04-14 09:40:33 +0900

    music_final

--- Lec04_Array_Class/music/music.cpp
+++ Lec04_Array_Class/music/music.cpp
@@ -7,8 +7,17 @@ int main()
 
 	my_service.addMusic("PolaroidLove", "ENHYPEN", "DIMENSION", 2022);
 	my_service.addMusic("Ditto", "NewJeans", "ALBUM", 2023);
+	my_service.addMusic("Test12", "NewJeans", "ALBUM", 2023);
 	my_service.addMusic("CAKE", "ITZY", "ALBUM", 2023);
 	my_service.addMusic("SundayMorning", "Maroon5", "ALBUM", 2023);
+	my_service.addMusic("Sugar", "Maroon5", "ALBUM", 2015);
+	my_service.addMusic("Test", "ITZY", "ALBUM", 2023);
+
+	// add by user 직접 해보기 
+	// music 객체 선언 --> 사용자 입력받기 (cin 사용)
+	// 선언한 music 객체 멤버수를 하나씩 채우기
+	// 마지막으로 music 객체 my_service의 music_list에 push_back()
+
 
 	// search music by title
 	string music_title;
@@ -21,5 +30,21 @@ int main()
 	else
 	{
 		cout << "not found" << endl;
+	}
+
+	// search music by artist
+	string artist_name;
+	cout << "Enter the artist name: ";
+	cin >> artist_name;
+	vector <Music*> artist_result = my_service.searchByArtist(artist_name);
+	if (artist_result.size() > 0) {
+		cout << "Found" << artist_result.size() << "song by " << artist_name << " : " << endl;
+		for (int i = 0; i < artist_result.size(); i++) {
+			cout << artist_result[i]->getTitle() << endl;
+		}
+
+	}
+	else {
+		cout << "NotFound" << endl;
 	}
 }
--- Lec04_Array_Class/music/music.h
+++ Lec04_Array_Class/music/music.h
@@ -50,6 +50,10 @@ public:
 		this->service_name = service_name;
 	}
 	// 음악 정보를 추가할 수 있는 기능
+	// add by user 직접 해보기 
+	// music 객체 선언 --> 사용자 입력받기 (cin 사용)
+	// 선언한 music 객체 멤버수를 하나씩 채우기
+	// 마지막으로 music 객체 my_service의 music_list에 push_back()
 	void addMusic(string title, string artist, string album, int year) {
 		Music new_music(title, artist, album, year);
 		music_list.push_back(new_music);
@@ -68,5 +72,15 @@ public:
 	}
 
 	// 음악 찾아주는 서비스 (Artist)
+	vector<Music*> searchByArtist(string artist) {
+		vector <Music*> result;
+		for (int i = 0; i < music_list.size(); i++) {
+			if (music_list[i].getArtist() == artist) {
+				result.push_back(&music_list[i]);
+				// &의 의미는?
+			}
+		}
 
+		return result;
+	}
 };
